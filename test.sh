fef479c1484798fcf1cc42188f83de82927f5ae5
Hyun Suk Noh <hsnoh@philstar.biz>
2017-06-19 10:09:42 +0800

    Fix git repository
    - git rm nbproject/private/*
    - add .gitignore
    - add test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#./itemsync db.conf /storage/philstar/snapshot/phsystem.latest/articles.DBF /storage/philstar/snapshot/phsystem.latest/ARTICLES/ARTCOLOR/
+./itemsync db.conf /storage/phsystem/articles.DBF /storage/phsystem/ARTICLES/ARTCOLOR/
